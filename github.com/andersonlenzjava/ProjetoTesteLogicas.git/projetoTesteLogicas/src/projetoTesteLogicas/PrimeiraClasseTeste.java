aed5431c94b6c8c65436e71c40b77dc72cc39f1d
ander <ander@DESKTOP-G3BCR3T>
2022-05-11 16:38:35 -0300

    Variável global e local

--- github.com/andersonlenzjava/ProjetoTesteLogicas.git/projetoTesteLogicas/src/projetoTesteLogicas/PrimeiraClasseTeste.java
+++ github.com/andersonlenzjava/ProjetoTesteLogicas.git/projetoTesteLogicas/src/projetoTesteLogicas/PrimeiraClasseTeste.java
@@ -2,10 +2,16 @@ package projetoTesteLogicas;
 
 public class PrimeiraClasseTeste {
 	
+	static int variavelGlobalUm = 25;
+	
 	public static void main(String[] args) {
 		
-		
+		int maiorIdade = 18;
+		System.out.println("Valor variavel local = " + maiorIdade);
 		
 	}
 
+	public static void metodo2 () {
+		System.out.println("Valor da varável Global = " + variavelGlobalUm);
+	}
 }
