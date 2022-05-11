6fcfedcb91b0c26fea2beccd864d044e9f78cf1f
ander <ander@DESKTOP-G3BCR3T>
2022-05-11 19:02:45 -0300

    Verificação de if e else com entrada do console.

---
+++ github.com/andersonlenzjava/ProjetoTesteLogicas.git/projetoTesteLogicas/src/projetoTesteLogicas/OperaçõesLogicasIF_Else.java
@@ -0,0 +1,28 @@
+package projetoTesteLogicas;
+
+import javax.swing.JOptionPane;
+
+public class OperaçõesLogicasIF_Else {
+
+	public static void main(String[] args) {
+
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno");
+
+		String mediaAluno = JOptionPane.showInputDialog("Qual a média do aluno");
+
+		double mediaAluno2 = Double.parseDouble(mediaAluno);
+		/* converte para int*/
+
+		if (mediaAluno2 >= 70 && nome.equals("Anderson")) {
+			System.out.println("Parabéns você está aprovado");
+
+		} else if (mediaAluno2 < 70 && nome.equals("Anderson")) {
+			System.out.println("Aluno você está reprovado");
+
+		} else {
+			System.out.println("Aluno não encontrado");
+		}
+
+	}
+
+}
